808007200891dace788fdc4bf3ac239ff103fdcc
dipu-bd <dipu.sudipta@gmail.com>
2017-10-25 11:52:11 +0600

    Block potential ad urls

--- app/src/main/java/lab/cupid/mangafox/MainActivity.java
+++ app/src/main/java/lab/cupid/mangafox/MainActivity.java
@@ -1,7 +1,8 @@
 package lab.cupid.mangafox;
 
-import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,25 +10,39 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.webkit.JsPromptResult;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import lab.cupid.mangafox.utils.AdBlocker;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String MANGAFOX_URL = "https://m.mangafox.me/";
 
+    // Current context
     private MainActivity mContext;
 
+    // Layout components
     private WebView mWebView;
     private ProgressBar mProgress;
 
+    // Provides some urls to block
+    private AdBlocker mAdBlocker;
+
+    // If it is true, the application shall exit on back pressed event
     private boolean mExitPending = false;
 
     @Override
@@ -54,7 +69,11 @@ public class MainActivity extends AppCompatActivity {
         settings.setUseWideViewPort(true);
         settings.setLoadWithOverviewMode(true);
         // Enable JavaScript
-        //settings.setJavaScriptEnabled(true);
+        settings.setJavaScriptEnabled(true);
+
+        // Load AdBlocker
+        mAdBlocker = new AdBlocker(this);
+        mAdBlocker.execute();
 
         // Load MangaFox
         if (savedInstanceState != null) {
@@ -103,13 +122,63 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
+     * Customized WebViewClient to handle errors
+     */
+    private class WebClient extends WebViewClient {
+        // To cache loaded urls
+        private final Map<String, Boolean> mLoadedUrls = new HashMap<>();
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            // Ignore SSL certificate errors
+            handler.proceed();
+
+            // Log the error message for debugging
+            Log.d(getClass().getSimpleName(), error.toString());
+        }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
+
+            // Log the error message for debugging
+            Log.d(getClass().getSimpleName(), error.toString());
+
+            // Display toast message describing error details
+            String errorDetails = String.valueOf(error.getDescription());
+            Toast.makeText(mContext, errorDetails, Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            Log.d(getClass().getSimpleName(), "Loading page: " + url);
+        }
+
+        @Override
+        public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+            // Uses cache to determine if the url is ad,
+            // and returns an empty resource for ads.
+            boolean ad;
+            if (!mLoadedUrls.containsKey(url)) {
+                ad = mAdBlocker.isAd(url);
+                mLoadedUrls.put(url, ad);
+            } else {
+                ad = mLoadedUrls.get(url);
+            }
+
+            Log.v(getClass().getSimpleName(), (ad ? "Blocked" : "Pass") + ": " + Uri.parse(url).getHost());
+
+            return ad ? mAdBlocker.createEmptyResource() :
+                    super.shouldInterceptRequest(view, url);
+        }
+    }
+
+    /**
      * Customized ChromeClient to display progress changes.
      */
     private class ChromeClient extends WebChromeClient {
         @Override
         public void onProgressChanged(WebView view, int newProgress) {
-            super.onProgressChanged(view, newProgress);
-
             // Make progress bar visible only when the client is loading.
             if(newProgress < 100) {
                 mProgress.setVisibility(View.VISIBLE);
@@ -127,35 +196,15 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onReceivedTitle(WebView view, String title) {
-            super.onReceivedTitle(view, title);
+            // Replace activity title with new title
             mContext.setTitle(title);
         }
-    }
-
-    /**
-     * Customized WebViewClient to handle errors
-     */
-    private class WebClient extends WebViewClient {
 
         @Override
-        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-            // Ignore SSL certificate errors
-            handler.proceed();
-
-            // Log the error message for debugging
-            Log.d(MainActivity.class.getSimpleName(), error.toString());
-        }
-
-        @Override
-        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-            super.onReceivedError(view, request, error);
-
-            // Log the error message for debugging
-            Log.d(MainActivity.class.getSimpleName(), error.toString());
-
-            // Display toast message describing error details
-            String errorDetails = String.valueOf(error.getDescription());
-            Toast.makeText(mContext, errorDetails, Toast.LENGTH_LONG).show();
+        public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+            Log.v(getClass().getSimpleName(), consoleMessage.message());
+            return super.onConsoleMessage(consoleMessage);
         }
     }
+
 }
